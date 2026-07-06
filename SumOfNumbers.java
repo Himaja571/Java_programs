7ca87439373acceb28d7eddf269dc5bd3ba48600
Himaja Chanda <himajapatel0571@gmail.com>
2026-07-06 19:30:52 +0530

    sum of two digits in java

---
+++ SumOfNumbers.java
@@ -0,0 +1,13 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        // Define two numbers
+        int number1 = 15;
+        int number2 = 25;
+        
+        // Calculate the sum
+        int sum = number1 + number2;
+        
+        // Display the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
